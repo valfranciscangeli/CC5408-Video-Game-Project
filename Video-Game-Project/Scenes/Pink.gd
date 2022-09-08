27511cb5eff16a01b59b5f20fb965438144a87ac
Valeria Franciscangeli <vvfranciscangeli@gmail.com>
2022-09-07 22:48:07 -0300

    escena level 1 y animacion idle
    se agrega una escena level 1 y una main donde luego iria el menu de inicio y se agrega animacion de idle con animation tree para darle un poco de movimiento al juego

--- Video-Game-Project/Scenes/Pink.gd
+++ Video-Game-Project/Scenes/Pink.gd
@@ -1,29 +1,30 @@
 extends KinematicBody2D
 
+
+# physics variables =======
 var velocity = Vector2()
 
 var ACCELERATION = 100
 var SPEED = 200
 var JUMP_SPEED = 300
 var GRAVITY = 10
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
+# sprites ========
+onready var sprite = $PinkyIdle
+onready var anim_player = $AnimationPlayer
+onready var anim_tree = $AnimationTree
+onready var playback = anim_tree.get("parameters/playback")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	anim_tree.active = true
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	var move_input = Input.get_axis("move_left", "move_right")
+	
 	
 	velocity.x = move_toward(velocity.x, move_input * SPEED, ACCELERATION)
 	
@@ -32,3 +33,6 @@ func _physics_process(delta):
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = -JUMP_SPEED
 	
+	#Animations
+	if abs(velocity.x):
+		playback.travel("idle")
