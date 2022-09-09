c3b518e16e4e0fd7129a390a79ee6ceadb2be4c9
Marcelo Valenzuela B <mavb2001@gmail.com>
2022-09-09 14:31:28 -0300

    branch creada para trabajar en animaciones de correr e idle

--- Video-Game-Project/Scenes/Pink.gd
+++ Video-Game-Project/Scenes/Pink.gd
@@ -2,10 +2,15 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 
+# Physics constans.
 var ACCELERATION = 100
 var SPEED = 200
 var JUMP_SPEED = 300
 var GRAVITY = 10
+
+onready var pivot = $Pivot
+onready var anim_tree = $AnimationTree
+onready var playback = anim_tree.get("parameters/playback")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -13,7 +18,7 @@ var GRAVITY = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	anim_tree.active = true # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,3 +37,14 @@ func _physics_process(delta):
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = -JUMP_SPEED
 	
+	# Animations
+	
+	if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
+		pivot.scale.x = 1
+	if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		pivot.scale.x = -1
+		
+	if abs(velocity.x) > 10:
+		playback.travel("run")
+	else:
+		playback.travel("idle")
