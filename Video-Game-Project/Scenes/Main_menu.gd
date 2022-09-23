661e4f17b3a4a97b0fd72ea852ebd95cd20fde9e
Francisco Almeida <fr.almeida.d@gmail.com>
2022-09-23 03:23:32 -0300

    main_menu
    i added the mainmenu ,and i created the resources folder and i added a font folder.

---
+++ Video-Game-Project/Scenes/Main_menu.gd
@@ -0,0 +1,17 @@
+extends MarginContainer
+onready var playmap1 = $VBoxContainer/Playmap1
+onready var playmap2 = $VBoxContainer/Playmap2
+onready var exit = $VBoxContainer/Exit
+
+
+
+func _ready():
+	exit.connect("pressed",self,"_on_exit_pressed")
+	playmap1.connect("pressed",self,"_on_playmap1_pressed")
+	
+func _on_exit_pressed():
+	get_tree().quit()
+func _on_playmap1_pressed():
+	get_tree().change_scene("res://Scenes/level01.tscn")
+	
+
