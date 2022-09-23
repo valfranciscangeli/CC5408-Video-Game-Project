62c2f729b6b68c4c369f94ebc79c1dd7a100f8ad
Marcelo Valenzuela B <mavb2001@gmail.com>
2022-09-22 22:12:02 -0300

    Añadiendo demo de la funcionalidad de agarre de items

--- Video-Game-Project/Scenes/Pink.gd
+++ Video-Game-Project/Scenes/Pink.gd
@@ -13,6 +13,7 @@ var GRAVITY = 10
 # sprites ========
 onready var pivot = $Pivot
 onready var sprite = $Pivot/PinkMonsterIdle4
+onready var item = $Pivot/Item
 onready var anim_player = $AnimationPlayer
 onready var anim_tree = $AnimationTree
 onready var playback = anim_tree.get("parameters/playback")
@@ -39,8 +40,10 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
 		pivot.scale.x = 1
+		item.scale.x = 1
 	if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
 		pivot.scale.x = -1
+		item.scale.x = -1
 		
 	if abs(velocity.x) > 0:
 		playback.travel("run")
