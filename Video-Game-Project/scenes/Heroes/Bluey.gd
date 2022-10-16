c29737b7edf54b0c3272e4af4a594adfb9f4a1f6
Valeria Franciscangeli <vvfranciscangeli@gmail.com>
2022-10-16 20:44:30 -0300

    Create Bluey.gd

---
+++ Video-Game-Project/scenes/Heroes/Bluey.gd
@@ -0,0 +1,18 @@
+extends "res://scenes/Heroes/Pink.gd"
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	move_right_action = "move_right3"
+	move_left_action = "move_left3"
+	jump_action = "jump3"
+	pick_action = "pick3"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
