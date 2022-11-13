6c060cb35933b90abe8f1ac9010fbd86edb576e1
Marcelo Valenzuela Barrera <mavb2001@gmail.com>
2022-11-13 19:51:21 -0300

    Boton añadido al inicio para acceder al nuevo mapa

--- Video-Game-Project/scenes/UI/Main_menu.gd
+++ Video-Game-Project/scenes/UI/Main_menu.gd
@@ -3,7 +3,7 @@ onready var play = $VBoxContainer/Play
 onready var credits = $VBoxContainer/Credits
 onready var exit = $VBoxContainer/Exit
 onready var tutorial = $VBoxContainer/Tutorial
-
+onready var map3 = $VBoxContainer/Map3
 
 
 func _ready():
@@ -11,6 +11,7 @@ func _ready():
 	play.connect("pressed",self,"_on_play_pressed")
 	credits.connect("pressed",self,"_on_credits_pressed")
 	tutorial.connect("pressed", self, "_on_tutorial_pressed")
+	map3.connect("pressed", self, "_on_map3_pressed")
 	
 func _on_exit_pressed():
 	get_tree().quit()
@@ -26,3 +27,7 @@ func _on_credits_pressed():
 func _on_tutorial_pressed():
 	if get_tree().change_scene("res://scenes/UI/Tutorial/part1.tscn") != OK:
 		print ("An unexpected error occured when trying to switch to the Tutorial part 1 scene")
+
+func _on_map3_pressed():
+	if get_tree().change_scene("res://scenes/Levels/level03.tscn") != OK:
+		print ("An unexpected error occured when trying to switch to the Level03 scene")
