645137b3ca6f88e06e7105efe136ff06e64a3097
Francisco Almeida Diaz <fr.almeida.d@gmail.com>
2022-11-14 00:27:39 -0300

    Revert "arreglando colisiones"
    This reverts commit 7c4cf1f04d67f2d1aec8a6e00fd6dbe08a265067.

--- Video-Game-Project/scenes/Items/block1.gd
+++ Video-Game-Project/scenes/Items/block1.gd
@@ -2,10 +2,7 @@ extends RigidBody2D
 onready var collision = get_node("CollisionShape2D")
 onready var picked = false
 onready var SPEED = 200
-onready var item = null
-export var velocity_to_throw = 100
-
-
+onready var item = get_node("../FollowerCamera/Pinky/Pivot/Item")
 
 func _integrate_forces(_state):
 	rotation_degrees = 10
@@ -15,50 +12,33 @@ func _physics_process(_delta):
 		collision.disabled = true
 		self.position = item.global_position
 		sleeping = true
-		if linear_velocity.length_squared() < velocity_to_throw : 
-			set_collision_layer_bit(3,false)
-			
+		
 	else:
 		collision.disabled = false
 		sleeping = false
 
 func _input(_event):
-	if _event.is_action_pressed("pick"):
-		if picked:
-			to_throw_objects("Pinky")
-		else:
-			pick_objects("Pinky")
-	elif _event.is_action_pressed("pick2"):
-		if picked:
-			to_throw_objects("Wooly")
-		else:
-			pick_objects("Wooly")
-	elif _event.is_action_pressed("pick3"):
-		if picked:
-			to_throw_objects("Bluey")
-		else:
-			pick_objects("Bluey")
+	if Input.is_action_just_pressed("pick") and picked == false:
+		var bodies = $detector.get_overlapping_bodies()
+		for b in bodies:
+			if b.name == "Pinky" and picked == false:
+				picked = true
+	elif Input.is_action_just_pressed("pick") and picked == true:
+		var bodies = $detector.get_overlapping_bodies()
+		for b in bodies:
+			if b.name == "Pinky" and picked == true:
+				picked = false
+				self.linear_velocity.x = 200 * item.scale.x
 				
-	
-#funciones necesarias para tomar y arrojar objetos 
-func to_throw_objects(player_name):
-	var bodies = $detector.get_overlapping_bodies()
-	for b in bodies:
-		if  b.name == player_name:
-			#activamos capa de colision de objeto lanzado 
-			set_collision_layer_bit(3,true)
-			picked = false
-			linear_velocity.x = 200 * item.scale.x
 
-func pick_objects(player_name):
-	
-	var bodies = $detector.get_overlapping_bodies()
-	for b in bodies:
 		
-		if  b.name == player_name:
-			item = b.get_node("Pivot/Item")
-			picked = true
-#---------------------------------------------------------
+		
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
